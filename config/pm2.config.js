bcca6dd5a29ab49f0a334600b65915b184b3e88d
cci <ccamensuli@gmail.com>
2017-07-13 08:15:13 +0200

    pm2 config file

---
+++ config/pm2.config.js
@@ -0,0 +1,26 @@
+const cpu = require('os').cpus().length ;
+
+module.exports = {
+  apps : [{
+    name                : "nodefony",
+    script              : "./nodefony",
+    watch               : false,
+    exec_mode		        : "cluster",
+    instances           : cpu,
+    max_memory_restart  : "1024M",
+    autorestart         : true,
+    max_restarts		    : 10,
+    log_file            : "./tmp/nodefony.log",
+    out_file            : "./tmp/nodefony.out.log",
+    error_file          : "./tmp/nodefony.error.log",
+    merge_logs          : true,
+    env: {
+      "NODE_ENV"        : "development",
+      "MODE_START"      : "PM2"
+    },
+    env_production : {
+       "NODE_ENV"       : "production",
+       "MODE_START"     : "PM2"
+    }
+  }]
+}
