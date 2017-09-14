1994e67d0af323142aa4bdf764bf3a66bbd78e5c
cci <ccamensuli@gmail.com>
2017-09-14 13:34:31 +0200

    add details connectors sequelize

--- src/nodefony/bundles/monitoringBundle/Entity/requestsEntity.js
+++ src/nodefony/bundles/monitoringBundle/Entity/requestsEntity.js
@@ -1,7 +1,7 @@
 /*
  *
  *
- *	ENTITY REQUESTS
+ *    ENTITY REQUESTS
  *
  *
  */
@@ -9,33 +9,33 @@ var Sequelize =require("sequelize");
 
 module.exports = nodefony.registerEntity("requests", function(){
 
-	var requests = function(db/*, ormService*/){
+    var requests = function(db/*, ormService*/){
 
-		var model = db.define("requests", {
-				id		:	{type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-				remoteAddress	:	{type:Sequelize.STRING },
-				userAgent	:	{type:Sequelize.STRING },
-				url		:	{type: Sequelize.TEXT },
-				route		:	{type:Sequelize.STRING },
-				method		:	{type:Sequelize.STRING },
-				state		:	{type:Sequelize.STRING },
-				protocole	:	{type:Sequelize.STRING },
-				username	:	{type:Sequelize.STRING },
-				data		:	{type: Sequelize.TEXT },
-		});
+        var model = db.define("requests", {
+                id        :    {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+                remoteAddress    :    {type:Sequelize.STRING },
+                userAgent    :    {type:Sequelize.STRING },
+                url        :    {type: Sequelize.TEXT },
+                route        :    {type:Sequelize.STRING },
+                method        :    {type:Sequelize.STRING },
+                state        :    {type:Sequelize.STRING },
+                protocole    :    {type:Sequelize.STRING },
+                username    :    {type:Sequelize.STRING },
+                data        :    {type: Sequelize.TEXT },
+        });
 
-		/*ormService.listen(this, 'onReadyConnection', function(connectionName, db, ormService){
-			if(connectionName == 'nodefony'){
+        /*ormService.listen(this, 'onReadyConnection', function(connectionName, db, ormService){
+            if(connectionName == 'nodefony'){
 
-			}
-		});*/
-		return model ;
-	};
+            }
+        });*/
+        return model ;
+    };
 
-	return {
-		type:"sequelize",
-		connection : "nodefony",
-		entity:requests
+    return {
+        type:"sequelize",
+        connection : "nodefony",
+        entity:requests
 
-	};
+    };
 });
