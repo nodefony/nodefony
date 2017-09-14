588719e7d4c5005f2c5fa2c914fe1e3c751fde6e
cci <ccamensuli@gmail.com>
2017-09-14 13:14:24 +0200

    debug sequelize mysql Sequelize.STRING(126).BINARY

--- src/nodefony/bundles/sequelizeBundle/Entity/clientEntity.js
+++ src/nodefony/bundles/sequelizeBundle/Entity/clientEntity.js
@@ -1,42 +1,42 @@
 /*
  *
  *
- *	ENTITY USER
+ *    ENTITY USER
  *
  *
  */
 try {
-	var Sequelize =require("sequelize");
+    var Sequelize =require("sequelize");
 } catch (err) {
   console.log('sequelize  is disabled! ' + err);
 }
 
 module.exports = nodefony.registerEntity("client", function(){
 
-	var Client = function(db, ormService){
-		var model = db.define("client", {
-				id		 	: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-				name 		: {type: Sequelize.STRING, unique: true, allowNull: false},
-				clientId	: {type: Sequelize.STRING , allowNull: false },
-				clientSecret: {type: Sequelize.STRING , allowNull: false },
-				isTrusted	: {type: Sequelize.STRING , allowNull: false }
+    var Client = function(db, ormService){
+        var model = db.define("client", {
+                id             : {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+                name         : {type: Sequelize.STRING(126).BINARY, unique: true, allowNull: false},
+                clientId    : {type: Sequelize.STRING , allowNull: false },
+                clientSecret: {type: Sequelize.STRING , allowNull: false },
+                isTrusted    : {type: Sequelize.STRING , allowNull: false }
 
-    		},{
-			classMethods: {
-			}
-		});
+            },{
+            classMethods: {
+            }
+        });
 
-		ormService.listen(this, 'onReadyConnection', function(connectionName, db, ormService){
-			if(connectionName == 'nodefony'){
+        /*ormService.listen(this, 'onReadyConnection', function(connectionName, db, ormService){
+            if(connectionName === 'nodefony'){
 
-			}
-		});
-		return model ;
-	};
+            }
+        });*/
+        return model ;
+    };
 
-	return {
-		type:"sequelize",
-		connection : "nodefony",
-		entity:Client
-	};
+    return {
+        type:"sequelize",
+        connection : "nodefony",
+        entity:Client
+    };
 });
