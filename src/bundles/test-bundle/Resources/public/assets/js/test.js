01a5dcaf719f4cff91ff6e82d4a51457d9305051
cci <ccamensuli@gmail.com>
2018-06-01 11:28:22 +0200

    debuf bundle name

--- src/bundles/test-bundle/Resources/public/assets/js/test.js
+++ src/bundles/test-bundle/Resources/public/assets/js/test.js
@@ -87,7 +87,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "testBundle/assets/";
+/******/ 	__webpack_require__.p = "test-bundle/assets/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
