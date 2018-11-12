d9b3643cefa1cfee0bfcdc98579cb1885fb4973d
cci <ccamensuli@gmail.com>
2018-11-12 08:47:44 +0100

    publish bash script

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,117 +7,149 @@ pathdemo=$(pwd)"/src/bundles";
 pathnodefony=$(pwd)"/src/nodefony";
 declare -a packages=();
 
-cd $pathproject
+outdated_packages () {
+  cd $pathproject
+  echo "Do you wish to search outdated packages nodefony $version ?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        nodefony outdated ;
+        break ;;
+      No )
+        break ;;
+    esac
+  done
+}
+
+compile_packages () {
+  cd $pathproject
+  echo "Do you wish compile (dump) webpack project bundles ?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        nodefony webpack:dump
+        break ;;
+      No )
+        break ;;
+    esac
+  done
+}
+
+publish_core_bundles () {
+
+  for file in $(ls $pathbundles ); do
+    if grep -q ".*-bundle" <<< $file &>/dev/null; then
+      packages=(${packages[@]} $file);
+      echo "PUBLISH CORE BUNDLES : @nodefony/$file ";
+      publish $pathbundles/$file
+    fi
+  done
+}
+
+publish_demo_bundles () {
+  cd $pathproject ;
+  for file in $(ls $pathdemo ); do
+    if grep -q "demo-bundle" <<< $file &>/dev/null; then
+      echo "PUBLISH BUNDLE : @nodefony/$file";
+      publish $pathdemo/$file ;
+    fi
+  done
+}
+
+publish_nodefony () {
+  echo "Ready to publish nodefony release : $version ?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        echo "PUBLISH NODEFONY $version";
+        publish $pathnodefony ;
+        break ;;
+      No )
+        break ;;
+    esac
+  done
+}
+
+publish () {
+  cd $1 ;
+  echo "CLEAN $1 DIRECTORY";
+  rm -rf node_modules ;
+  yarn;
+  npm install;
+  #npm publish --access=public;
+}
+
+upgrade_nodefony () {
+  cd $pathnodefony;
+  for package in ${packages[@]}; do
+    echo "UPGRADE PACKAGE nodefony with : @nodefony/$package";
+    #npm install @nodefony/$package
+  done
+}
+
+commit_project () {
+  cd $pathproject;
+  git status
+  echo "Do you wish to commit this nodefony release $version ?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        echo "COMMIT PROJECT $version";
+        #git commit -a -m nodefony release $version
+        echo "PUSH PROJECT TO REMOTE";
+        #git push
+        echo "TAG PROJECT v$version";
+        #git tag v$version
+        #git tag
+        #git push origin v$version
+        break ;;
+      No )
+        break ;;
+    esac
+  done
+}
+
+tools (){
+  cd $pathproject
+  echo "Do you wish to copy README and CHANGELOG project in nodefony package ?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        echo "copy README.md";
+        #cp README.md $pathnodefony ;
+        echo "copy CHANGELOG.md";
+        #cp CHANGELOG.md $pathnodefony ;
+        break ;;
+      No )
+        break ;;
+    esac
+  done
+}
+
+generate_changelog () {
+  cd $pathproject
+  ./node_modules/.bin/gren changelog --username=nodefony  --repo=nodefony-core --tags=$version --data-source=commits --generate  --override
+}
+
+##########
+#  Main  #
+##########
 # outdated packages
-echo "Do you wish to search outdated packages nodefony $version ?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes )
-      nodefony outdated ;
-      break ;;
-    No )
-      break ;;
-  esac
-done
-
-# WEBPACK
-echo "Do you wish compile (dump) webpack project bundles ?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes )
-      nodefony webpack:dump
-      break ;;
-    No )
-      break ;;
-  esac
-done
-
+outdated_packages
+# webpack
+compile_packages
 # PUBLISH CORE BUNDLES
-cd $pathbundles ;
-for file in $(ls $pathbundles ); do
-  if grep -q ".*-bundle" <<< $file &>/dev/null; then
-    cd $pathbundles/$file;
-    packages=(${packages[@]} $file);
-    echo "CLEAN $pathbundles/$file DIRECTORY";
-    rm -rf node_modules
-    yarn
-    npm install
-    echo "PUBLISH CORE BUNDLES : @nodefony/$file ";
-    #npm publish --access=public
-  fi
-done
-
+publish_core_bundles
 # PUBLISH DEMO BUNDLE
-cd $pathproject ;
-for file in $(ls $pathdemo ); do
-  if grep -q "demo-bundle" <<< $file &>/dev/null; then
-    cd $pathdemo/$file;
-    echo "CLEAN $pathdemo/$file DIRECTORY";
-    rm -rf node_modules
-    yarn
-    npm install
-    echo "PUBLISH BUNDLE : @nodefony/$file";
-    #npm publish --access=public
-  fi
-done
+publish_demo_bundles
+
+sleep 2;
 
 # UPGRADE NODEFONY
-sleep 2;
-cd $pathnodefony;
-for package in ${packages[@]}; do
-  echo "UPGRADE PACKAGE nodefony with : @nodefony/$package";
-  #npm install @nodefony/$package
-done
-
+upgrade_nodefony
 # TOOLS
-cd $pathproject
-echo "Do you wish to copy README and CHANGELOG project in nodefony package ?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes )
-      echo "copy README.md";
-      #cp README.md $pathnodefony ;
-      echo "copy CHANGELOG.md";
-      #cp CHANGELOG.md $pathnodefony ;
-      break ;;
-    No )
-      break ;;
-  esac
-done
-
+tools
 # COMMIT PROJECT
-cd $pathproject;
-git status
-echo "Do you wish to commit this nodefony release $version ?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes )
-      echo "COMMIT PROJECT $version";
-      #git commit -a -m nodefony release $version
-      echo "TAG PROJECT v$version";
-      #git tag v$version
-      #git tag
-      #git push origin v$version
-      break ;;
-    No )
-      break ;;
-  esac
-done
-
+commit_project
 # PUBLISH NODEFONY
-echo "Ready to publish nodefony release : $version ?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes )
-      cd $pathnodefony ;
-      echo "CLEAN NODEFONY REPOSITORY";
-      rm -rf node_modules
-      yarn
-      npm install
-      echo "PUBLISH NODEFONY $version";
-      #npm publish --access=public
-      break ;;
-    No )
-      break ;;
-  esac
-done
+publish_nodefony
