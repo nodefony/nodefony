7a930bbcd2e7e713631ca531fcd030f749eb7007
cci <ccamensuli@gmail.com>
2018-11-12 15:01:53 +0100

    nodefony release 4.0.0-beta.12

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-version=`nodefony version`;
-pathproject=$(pwd);
-pathbundles=$(pwd)"/src/nodefony/bundles";
-pathdemo=$(pwd)"/src/bundles";
-pathnodefony=$(pwd)"/src/nodefony";
-declare -a packages=();
-
 outdated_packages () {
   cd $pathproject
   echo "Do you wish to search outdated packages nodefony $version ?"
@@ -35,13 +28,21 @@ compile_packages () {
   done
 }
 
-publish_core_bundles () {
-
+add_bundle () {
   for file in $(ls $pathbundles ); do
     if grep -q ".*-bundle" <<< $file &>/dev/null; then
       packages=(${packages[@]} $file);
+    fi
+  done
+}
+
+publish_core_bundles () {
+  for file in $(ls $pathbundles ); do
+    if grep -q ".*-bundle" <<< $file &>/dev/null; then
+      npm_version;
+      #packages=(${packages[@]} $file);
       echo "PUBLISH CORE BUNDLES : @nodefony/$file ";
-      publish $pathbundles/$file
+      npm_publish $pathbundles/$file
     fi
   done
 }
@@ -50,8 +51,18 @@ publish_demo_bundles () {
   cd $pathproject ;
   for file in $(ls $pathdemo ); do
     if grep -q "demo-bundle" <<< $file &>/dev/null; then
+      cd $pathdemo/$file;
+      npm_version;
+      echo "COMMIT DEMO-BUNDLE $version";
+      git commit -a -m demo-bundle release $version
+      echo "PUSH DEMO-BUNDLE TO REMOTE";
+      git push
+      echo "TAG DEMO-BUNDLE v$version";
+      git tag v$version
+      git tag
+      git push origin v$version
       echo "PUBLISH BUNDLE : @nodefony/$file";
-      publish $pathdemo/$file ;
+      npm_publish $pathdemo/$file ;
     fi
   done
 }
@@ -62,7 +73,7 @@ publish_nodefony () {
     case $yn in
       Yes )
         echo "PUBLISH NODEFONY $version";
-        publish $pathnodefony ;
+        npm_publish $pathnodefony ;
         break ;;
       No )
         break ;;
@@ -70,38 +81,30 @@ publish_nodefony () {
   done
 }
 
-publish () {
-  cd $1 ;
-  echo "CLEAN $1 DIRECTORY";
-  rm -rf node_modules ;
-  yarn;
-  npm install;
-  #npm publish --access=public;
-}
-
 upgrade_nodefony () {
   cd $pathnodefony;
+  npm_version;
+  add_bundle;
   for package in ${packages[@]}; do
     echo "UPGRADE PACKAGE nodefony with : @nodefony/$package";
-    #npm install @nodefony/$package
+    npm install @nodefony/$package
   done
 }
 
 commit_project () {
   cd $pathproject;
-  git status
   echo "Do you wish to commit this nodefony release $version ?"
   select yn in "Yes" "No"; do
     case $yn in
       Yes )
         echo "COMMIT PROJECT $version";
-        #git commit -a -m nodefony release $version
+        git commit -a -m "nodefony release $version"
         echo "PUSH PROJECT TO REMOTE";
-        #git push
+        git push
         echo "TAG PROJECT v$version";
-        #git tag v$version
-        #git tag
-        #git push origin v$version
+        git tag v$version
+        git tag
+        git push origin v$version
         break ;;
       No )
         break ;;
@@ -109,16 +112,38 @@ commit_project () {
   done
 }
 
+npm_publish () {
+  cd $1 ;
+  echo "CLEAN $1 DIRECTORY";
+  rm -rf node_modules ;
+  yarn;
+  npm install;
+  npm publish --access=public;
+}
+
+npm_version () {
+  if [ $1 ]
+  then
+    npm version $1 --allow-same-version --git-tag-version false
+  else
+    npm version $version --allow-same-version --git-tag-version false
+  fi
+}
+
 tools (){
   cd $pathproject
-  echo "Do you wish to copy README and CHANGELOG project in nodefony package ?"
+  echo "Do you wish to copy README and Generate CHANGELOG project in nodefony package ?"
   select yn in "Yes" "No"; do
     case $yn in
       Yes )
         echo "copy README.md";
-        #cp README.md $pathnodefony ;
+        cp README.md $pathnodefony ;
+        generate_changelog
         echo "copy CHANGELOG.md";
-        #cp CHANGELOG.md $pathnodefony ;
+        cp CHANGELOG.md $pathnodefony ;
+        echo "COMMIT CHANGELOG PROJECT $version";
+        git commit -a -m nodefony changelog $version
+        git push
         break ;;
       No )
         break ;;
@@ -128,28 +153,106 @@ tools (){
 
 generate_changelog () {
   cd $pathproject
-  ./node_modules/.bin/gren changelog --username=nodefony  --repo=nodefony-core --tags=$version --data-source=commits --generate  --override
+  mv CHANGELOG.md CHANGELOG.md.old
+  if [ $1 ]
+  then
+    ./node_modules/.bin/gren changelog --username=nodefony  --repo=nodefony-core --tags=$1 --data-source=commits --generate  --override
+  else
+    ./node_modules/.bin/gren changelog --username=nodefony  --repo=nodefony-core --tags=$version --data-source=commits --generate  --override
+  fi
+  sed -i "" 's,# Changelog,---,g' CHANGELOG.md.old
+  echo >> CHANGELOG.md;
+  cat CHANGELOG.md.old >> CHANGELOG.md ;
+  rm CHANGELOG.md.old ;
+}
+
+publish (){
+  # outdated packages
+  #outdated_packages
+  # webpack
+  compile_packages
+  # PUBLISH CORE BUNDLES
+  publish_core_bundles
+  # PUBLISH DEMO BUNDLE
+  publish_demo_bundles
+  sleep 10;
+  # UPGRADE NODEFONY
+  upgrade_nodefony
+  # COMMIT PROJECT
+  commit_project
+  # TOOLS CHANGELOG
+  tools
+  # PUBLISH NODEFONY
+  publish_nodefony
+}
+
+menu () {
+  declare -a commands=("publish" "changelog" "Quit");
+  # MENU
+  select yn in ${commands[@]}; do
+    case $yn in
+      "changelog" )
+        generate_changelog;
+        break ;;
+      "publish" )
+        publish ;
+        break ;;
+      "Quit" )
+        exit;
+    esac
+  done
 }
 
 ##########
 #  Main  #
 ##########
-# outdated packages
-outdated_packages
-# webpack
-compile_packages
-# PUBLISH CORE BUNDLES
-publish_core_bundles
-# PUBLISH DEMO BUNDLE
-publish_demo_bundles
+clear;
 
-sleep 2;
+version=`nodefony version`;
+pathproject=$(pwd);
+pathbundles=$(pwd)"/src/nodefony/bundles";
+pathdemo=$(pwd)"/src/bundles";
+pathnodefony=$(pwd)"/src/nodefony";
+declare -a packages=();
 
-# UPGRADE NODEFONY
-upgrade_nodefony
-# TOOLS
-tools
-# COMMIT PROJECT
-commit_project
-# PUBLISH NODEFONY
-publish_nodefony
+echo "
+ _   _    ___    ____    _____   _____    ___    _   _  __   __
+| \ | |  / _ \  |  _ \  | ____| |  ___|  / _ \  | \ | | \ \ / /
+|  \| | | | | | | | | | |  _|   | |_    | | | | |  \| |  \ V /
+| |\  | | |_| | | |_| | | |___  |  _|   | |_| | | |\  |   | |
+|_| \_|  \___/  |____/  |_____| |_|      \___/  |_| \_|   |_|
+
+    version : $version
+    GIT tags :
+`git tag`
+    GIT status :
+`git status`
+";
+
+
+# PARSE ARGS
+case $1 in
+  "publish" )
+    publish ;
+    break ;;
+  "changelog" )
+    generate_changelog $2;
+    break ;;
+  "upgrade_nodefony" )
+    upgrade_nodefony;
+    break ;;
+  "commit_project" )
+    commit_project;
+    break ;;
+  * )
+    echo "
+    __  __
+   |  \/  |   ___   _ __    _   _
+   | |\/| |  / _ \ | '_ \  | | | |
+   | |  | | |  __/ | | | | | |_| |
+   |_|  |_|  \___| |_| |_|  \__,_|
+
+    ";
+    menu;
+    break ;;
+esac
