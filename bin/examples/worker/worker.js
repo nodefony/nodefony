f872eff6ba8209dde9019d074242186c1f2b844e
cci <ccamensuli@gmail.com>
2020-01-24 15:24:16 +0100

    create serviceWorker example

--- bin/examples/worker/worker.js
+++ bin/examples/worker/worker.js
@@ -6,82 +6,65 @@ try {
 } catch (e) {
   nodefony = require(require("path").resolve("src", "nodefony", "autoloader.es6"));
 }
-
 const {
   Worker,
   MessageChannel,
-  MessagePort,
+  //MessagePort,
   isMainThread,
   parentPort,
-  workerData
+  //workerData
 } = require('worker_threads');
 
 const path = require("path");
 const service = path.resolve(__dirname, "service.js");
 
 
-new nodefony.cli("MAIN", {
-  pid: true,
-  onStart: (cli) => {
-    //console.log(cli)
-    try {
-      const subChannel = new MessageChannel();
-      subChannel.port2.on('message', (value) => {
-        console.log('received:', value);
-      });
-      const worker = new Worker(service, {
-        workerData: {}
-      });
-      worker.on('online', () => {
-        cli.logger(`Worker online !`, "INFO", );
-      });
-      cli.logger(worker.threadId);
-      worker.postMessage(subChannel.port1, [subChannel.port1]);
+if (isMainThread) {
+  new nodefony.cli("MAIN", {
+      pid: true,
+      autostart: false
+    })
+    .start()
+    .then((cli) => {
+      try {
+        const subChannel = new MessageChannel();
+        subChannel.port2.on('message', (value) => {
+          console.log('received:', value);
+        });
+        const worker = new Worker(service, {
+          workerData: {}
+        });
+        //cli.logger(` WORKER ID : ${worker.threadId}`);
 
-      worker.on('message', (message) => {
-        cli.logger(message, "INFO", `MAIN EVENT MESSAGE`);
-      });
+        worker.on('online', () => {
+          cli.logger(`Worker online ! Thread ID : ${worker.threadId}`, "INFO");
+        });
+        //worker.postMessage(subChannel.port1, [subChannel.port1]);
+        setTimeout(() => {
+          worker.postMessage("sbbob");
+        }, 2000);
 
-      worker.on('error', (e) => {
+
+        worker.on('message', (message) => {
+          cli.logger(message, "INFO", `MAIN EVENT MESSAGE`);
+        });
+
+        worker.on('error', (e) => {
+          cli.logger(e, "ERROR");
+        });
+
+        worker.on('exit', (code) => {
+          if (code !== 0) {
+            throw new Error(`Worker stopped with exit code ${code}`);
+          }
+          cli.logger(code);
+        });
+
+        return worker;
+      } catch (e) {
         cli.logger(e, "ERROR");
-      });
-
-      worker.on('exit', (code) => {
-        if (code !== 0) {
-          throw new Error(`Worker stopped with exit code ${code}`);
-        }
-        cli.logger(code);
-      });
-    } catch (e) {
-      cli.logger(e, "ERROR");
-    }
-  }
-});
-
-
-
-
-
-
-/*else {
-  const script = workerData;
-  console.log(script)
-  parentPort.postMessage(script);
-}*/
-
-/*if (isMainThread) {
-  const worker = new Worker(service);
-  const subChannel = new MessageChannel();
-  worker.postMessage({
-    hereIsYourPort: subChannel.port1
-  }, [subChannel.port1]);
-  subChannel.port2.on('message', (value) => {
-    console.log('received:', value);
-  });
+      }
+    });
 } else {
-  parentPort.once('message', (value) => {
-    assert(value.hereIsYourPort instanceof MessagePort);
-    value.hereIsYourPort.postMessage('the worker is sending this');
-    value.hereIsYourPort.close();
-  });
-}*/
+  throw new Error("can't be worker");
+}
