6bf010962fc8b29a783203754a5fa6f95fc2ba19
cci <ccamensuli@gmail.com>
2020-04-26 00:53:32 +0200

    vue.js update config for HMR

--- src/nodefony/cli/builder/vue/skeletons/vue.config.js
+++ src/nodefony/cli/builder/vue/skeletons/vue.config.js
@@ -1,12 +1,18 @@
 // vue.config.js
 const path = require('path');
-
+const package = require(path.resolve("package.json"));
 const outputDir = path.resolve("Resources", "public");
 const indexPath = path.resolve("Resources", "views", 'index.html.twig');
 const publicPath = "/{{bundleName}}";
 const template = path.resolve('public', 'index.html');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+process.env.VUE_APP_VERSION = package.version;
+process.env.VUE_APP_DEBUG = process.env.DEBUG_MODE;
+process.env.VUE_APP_NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
+  lintOnSave: false,
   publicPath: publicPath,
   outputDir: outputDir,
   indexPath: indexPath,
@@ -18,5 +24,18 @@ module.exports = {
         args[0].template = template;
         return args;
       });
+  },
+  configureWebpack: {
+    output:{
+      hotUpdateChunkFilename: 'hot/hot-update.js',
+      hotUpdateMainFilename: 'hot/hot-update.json'
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [`${outputDir}/hot/*.hot-update.*`],
+        dry: false,
+        dangerouslyAllowCleanPatternsOutsideProject: true
+      })
+    ]
   }
 };
