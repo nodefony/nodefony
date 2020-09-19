baabca64e57e6386ec6b00b23cfc13d28e45979a
cci <ccamensuli@gmail.com>
2020-09-19 15:56:24 +0200

    debug graphiql

--- src/nodefony/cli/builder/bundles/users-bundle/Resources/graphiql/graphiql.jsx
+++ src/nodefony/cli/builder/bundles/users-bundle/Resources/graphiql/graphiql.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { render } from 'react-dom';
-import GraphiQL from 'graphiql';
-import 'graphiql/graphiql.min.css';
-
+import * as React from "react";
+import { render } from "react-dom";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.min.css";
+import "./index.css";
 const NODE_ENV = process.env.NODE_ENV ;
 const DEBUG = process.env.DEBUG ;
 const config = process.env.GRAPHIQL ;
+
+const URL = config.url;
+
+function graphQLFetcher(graphQLParams) {
+  return fetch(URL, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(graphQLParams),
+    credentials: 'same-origin',
+  }).then(response => response.json().then((ele)=>{
+    console.log(ele)
+    return ele.data;
+  }));
+}
+
+const defaultQuery = `
+{
+  user(username: "admin") {
+    username
+     surname
+     name
+   }
+   users {
+     username
+     name
+     surname
+     updatedAt
+     roles
+   }
+}
+`;
+
+const container = document.getElementById("graphiql");
 const Logo = () => {
   return <span className="graphiql-container" style={{width:"15%"}}>
     <img width="30px" heigth="30px" src={config.logo}></img>
@@ -18,28 +54,7 @@ const Logo = () => {
 // See GraphiQL Readme - Advanced Usage section for more examples like this
 GraphiQL.Logo = Logo;
 
-const App = () => (
-  <GraphiQL
-    style={{ height: '100vh' }}
-    fetcher={async graphQLParams => {
-      const data = await fetch(
-        config.url,
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(graphQLParams),
-          credentials: 'same-origin',
-        }
-      );
-      let res = await data.json().catch(() => data.text());
-      if (res.error){
-        return res;
-      }
-      return res.data ;
-    }}
-  />
+render(
+  <GraphiQL fetcher={graphQLFetcher} defaultQuery={defaultQuery} />,
+  container
 );
-render(<App />, document.getElementById('root'));
