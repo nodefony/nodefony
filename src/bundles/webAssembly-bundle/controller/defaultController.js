c9b17fc78f758171c9b4e27438e5f857f470d7eb
Christophe CAMENSULI <ccamensuli@gmail.com>
2021-06-18 14:58:52 +0200

    demo webassembly WASI

--- src/bundles/webAssembly-bundle/controller/defaultController.js
+++ src/bundles/webAssembly-bundle/controller/defaultController.js
@@ -1,5 +1,12 @@
 const binding = require('../build/Release/webAssembly.node');
 
+// npx --node-options=--experimental-wasi-unstable-preview1 --experimental-wasm-bigint nodefony dev
+let WASI = null
+try{
+  const wasi = require('wasi');
+  WASI = wasi.WASI;
+}catch(e){}
+
 /**
  *	@class defaultController
  *	@constructor
@@ -18,9 +25,9 @@ class defaultController extends nodefony.Controller {
 /**
  *    @Route ("",
  *      name="route-webAssembly-bundle-webAssembly")
+ *    @webpack  webassembly in webpack with wasm-loader
  */
   indexAction() {
-
     return this.render("webAssembly-bundle::index.html.twig", {
 			name: this.bundle.name,
 			description: this.bundle.package.description,
@@ -28,6 +35,38 @@ class defaultController extends nodefony.Controller {
     });
 
   }
+
+  /**
+   *    @Route ("/wasi",
+   *      name="route-wasi-bundle-webAssembly")
+   */
+    async wasiAction() {
+      const sandbox = path.resolve(__dirname, "..", "src", "webassembly");
+      const wasmFile = path.resolve(sandbox, "add.wasm");
+
+      const wasi = new WASI({
+        args: process.argv,
+        env: process.env,
+        preopens: {
+          '/sandbox': sandbox
+        }
+      });
+
+      const wasm = await WebAssembly.compile(fs.readFileSync(wasmFile));
+      const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
+      const instance = await WebAssembly.instantiate(wasm, importObject);
+
+      console.log(instance)
+      //wasi.start(instance);
+      console.log(instance.exports.add(2, 3));
+
+      return this.render("webAssembly-bundle::index.html.twig", {
+  			name: this.bundle.name,
+  			description: this.bundle.package.description,
+        binding: instance.exports.add(2, 3)
+      });
+
+    }
 }
 
 module.exports = defaultController;
