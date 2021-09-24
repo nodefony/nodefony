b0cac5db65b363810ee393adafd4bcba2e3e6434
Christophe CAMENSULI <ccamensuli@gmail.com>
2021-09-24 10:44:55 +0200

    upgrade deps

--- config/config.js
+++ config/config.js
@@ -107,7 +107,8 @@ module.exports = {
       "test-bundle": path.resolve("src", "bundles", "test-bundle"),
       "users-bundle": path.resolve("src", "nodefony", "cli", "builder", "bundles", "users-bundle"),
       //"demo-bundle": "file:src/bundles/demo-bundle",
-      //"webAssembly-bundle": "file:src/bundles/webAssembly-bundle"
+      //"webAssembly-bundle": "file:src/bundles/webAssembly-bundle",
+      //"ia-bundle": "file:src/bundles/ia-bundle"
     },
     /**
      * SYSLOG NODEFONY
