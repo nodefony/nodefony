96eb79f82a93b740a167b410dbab82d4782d3a26
Christophe CAMENSULI <ccamensuli@gmail.com>
2021-12-23 18:52:01 +0100

    upgrade redis service

---
+++ src/bundles/test-bundle/controller/unitTest/redis/redisController.js
@@ -0,0 +1,72 @@
+/**
+ *    @Route ("/test/redis")
+ */
+module.exports = class redisController extends nodefony.controller {
+
+  constructor(container, context) {
+    super(container, context);
+    this.redis = this.get("redis");
+    this.setJsonContext();
+    this.main = this.redis.getConnection("main").client;
+    this.publisher = this.redis.getConnection("publish").client;
+    this.subscriber = this.redis.getConnection("subscribe").client;
+  }
+
+
+  /**
+   *    @Method ({"GET"})
+   *    @Route ("/ping" , name="test-redis-ping")
+   *
+   */
+  async pingAction() {
+    let pong = await this.main.ping()
+    console.log(`Response from PING command: ${pong}`);
+    return this.renderJson({
+      message: pong
+    })
+  }
+
+  /**
+   *    @Method ({"GET"})
+   *    @Route ("/index" , name="test-redis-index")
+   *
+   */
+  async indexAction() {
+    await this.main.hSet('key', 'foo', 'bar');
+    let result = await this.main.hGetAll('key');
+    await this.main.hSet('key', 'sboob', 'cci');
+    let result2 = await this.main.hGetAll('key');
+    console.log(result, result2)
+    let message = {
+      foo: "bar"
+    }
+    try {
+      let res = await this.publisher.publish("send", JSON.stringify(message));
+      console.log(res)
+      return this.renderJson(result2)
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   *    @Method ({"GET"})
+   *    @Route ("/scan" , name="test-redis-scan")
+   *
+   */
+  async hscanAction() {
+    let tab = [];
+    try {
+      for await (const { field, value } of this.main.hScanIterator("key")) {
+        // use the key!
+        tab.push({field, value});
+      }
+      return this.renderJson(tab)
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+}
--- src/nodefony/bundles/redis-bundle/services/redisService.js
+++ src/nodefony/bundles/redis-bundle/services/redisService.js
@@ -16,22 +16,27 @@ class redisConnection extends nodefony.Service {
   }
 
   create(options = {}) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
-        this.settings = nodefony.extend({}, this.settings, options);
-        this.client = this.service.engine.createClient(options);
-        this.client.on('ready', () => {
-          this.fire("onReady", this);
-          if (options.password) {
-            this.client.auth(options.password, () => {
-              this.log(`AUTHENTICATED READY ${this.settings.host} : ${this.settings.port} `, "INFO");
-              return resolve(this.client);
-            });
+        this.settings = nodefony.extend({}, options);
+        this.client = this.service.engine.createClient(this.settings);
+        await this.client.connect();
+        if (options.password) {
+          if (options.username) {
+            await this.client.auth({
+              username: options.username,
+              password: options.password
+            })
           } else {
-            this.log(`READY ${this.settings.host} : ${this.settings.port} `, "INFO");
-            return resolve(this.client);
+            await this.client.auth({
+              password: options.password
+            })
           }
-        });
+          this.log(`AUTHENTICATED READY ${this.settings.socket.host} : ${this.settings.socket.port} `, "INFO");
+        } else {
+          this.log(`READY ${this.settings.socket.host} : ${this.settings.socket.port} `, "INFO");
+        }
+
         this.client.on('error', (error) => {
           this.log(error, "ERROR");
           this.fire("onError", error, this);
@@ -41,54 +46,57 @@ class redisConnection extends nodefony.Service {
           this.fire("onWarning", warning, this);
         });
         this.client.on('end', () => {
-          this.log(`END CONNECT   ${this.settings.host} : ${this.settings.port} `, "INFO");
+          this.log(`END CONNECT   ${this.settings.socket.host} : ${this.settings.socket.port} `, "INFO");
           this.fire("onEnd", this);
         });
         this.client.on('connect', () => {
-          this.log(`CONNECT  ${this.settings.host} : ${this.settings.port} `, "INFO");
+          this.log(`CONNECT  ${this.settings.socket.host} : ${this.settings.socket.port} `, "INFO");
           this.fire("onConnect", this);
         });
         this.client.on('reconnecting', () => {
-          this.log(`RECONNECTING  ${this.settings.host} : ${this.settings.port} `, "INFO");
+          this.log(`RECONNECTING  ${this.settings.socket.host} : ${this.settings.socket.port} `, "INFO");
           this.fire("onReconnecting", this);
         });
         this.client.on("subscribe", (channel, count) => {
-          this.log(`SUBSCRIBE  ${this.settings.host}:${this.settings.port} channel : ${channel} `, "INFO");
+          this.log(`SUBSCRIBE  ${this.settings.socket.host}:${this.settings.socket.port} channel : ${channel} `, "INFO");
           this.fire("onSubscribe", channel, count, this);
         });
         this.client.on("unsubscribe", (channel, count) => {
-          this.log(`UNSUBSCRIBE  ${this.settings.host}:${this.settings.port} channel : ${channel} `, "INFO");
+          this.log(`UNSUBSCRIBE  ${this.settings.socket.host}:${this.settings.socket.port} channel : ${channel} `, "INFO");
           this.fire("onUnsubscribe", channel, count, this);
         });
         this.client.on("psubscribe", (pattern, count) => {
-          this.log(`PSUBSCRIBE  ${this.settings.host}:${this.settings.port} pattern : ${pattern} `, "INFO");
+          this.log(`PSUBSCRIBE  ${this.settings.socket.host}:${this.settings.socket.port} pattern : ${pattern} `, "INFO");
           this.fire("onPsubscribe", pattern, count, this);
         });
         this.client.on("punsubscribe", (pattern, count) => {
-          this.log(`PUNSUBSCRIBE  ${this.settings.host}:${this.settings.port} pattern : ${pattern} `, "INFO");
+          this.log(`PUNSUBSCRIBE  ${this.settings.socket.host}:${this.settings.socket.port} pattern : ${pattern} `, "INFO");
           this.fire("onPunsubscribe", pattern, count, this);
         });
         this.client.on("message", (channel, message) => {
-          this.log(`MESSAGE  ${this.settings.host}:${this.settings.port} channel : ${channel} `, "DEBUG");
+          this.log(`MESSAGE  ${this.settings.socket.host}:${this.settings.socket.port} channel : ${channel} `, "DEBUG");
           this.log(message, "DEBUG");
           this.fire("onMessage", channel, message, this);
         });
         this.client.on("message_buffer", (channel, message) => {
-          this.log(`MESSAGE BUFFER  ${this.settings.host}:${this.settings.port} channel : ${channel} `, "DEBUG");
+          this.log(`MESSAGE BUFFER  ${this.settings.socket.host}:${this.settings.socket.port} channel : ${channel} `, "DEBUG");
           this.log(message, "DEBUG");
           this.fire("onMessage_buffer", channel, message, this);
         });
         this.client.on("pmessage", (pattern, channel, message) => {
-          this.log(`PMESSAGE  ${this.settings.host}:${this.settings.port} pattern : ${pattern} channel : ${channel} `, "DEBUG");
+          this.log(`PMESSAGE  ${this.settings.socket.host}:${this.settings.socket.port} pattern : ${pattern} channel : ${channel} `, "DEBUG");
           this.log(message, "DEBUG");
           this.fire("onPmessage", pattern, channel, message, this);
         });
         this.client.on("pmessage_buffer", (pattern, channel, message) => {
-          this.log(`PMESSAGE BUFFER  ${this.settings.host}:${this.settings.port} pattern : ${pattern} channel : ${channel} `, "DEBUG");
+          this.log(`PMESSAGE BUFFER  ${this.settings.socket.host}:${this.settings.socket.port} pattern : ${pattern} channel : ${channel} `, "DEBUG");
           this.log(message, "DEBUG");
           this.fire("onPmessage_buffer", pattern, channel, message, this);
         });
+        this.fire("onReady", this);
+        return resolve(this.client);
       } catch (e) {
+        console.trace(e)
         return reject(e);
       }
     });
@@ -100,18 +108,17 @@ class Redis extends nodefony.services.Connections {
 
   constructor(container) {
     super("redis", container, redis, redisConnection);
-    BlueBird.promisifyAll(this.engine);
-    this.degug = false;
+    this.debug = false;
   }
 
   readConfig() {
     this.settings = this.bundle.settings.redis;
-    this.settings.retry_strategy = this.retry_strategy;
     if (this.settings.debug) {
-      this.degug = true;
+      this.debug = true;
       process.env.DEBUG_MODE = "redis";
     }
     this.globalOptions = this.settings.globalOptions;
+    this.globalOptions.socket.reconnectStrategy = this.retry_strategy;
     for (let connection in this.settings.connections) {
       let options = nodefony.extend({}, this.globalOptions, this.settings.connections[connection]);
       this.createConnection(connection, options)
@@ -130,23 +137,8 @@ class Redis extends nodefony.services.Connections {
     return this.log(message);
   }
 
-  retry_strategy(options) {
-    if (options.error && options.error.code === 'ECONNREFUSED') {
-      // End reconnecting on a specific error and flush all commands with
-      // a individual error
-      return new Error('The server refused the connection');
-    }
-    if (options.total_retry_time > 1000 * 60 * 60) {
-      // End reconnecting after a specific timeout and flush all commands
-      // with a individual error
-      return new Error('Retry time exhausted');
-    }
-    if (options.attempt > 10) {
-      // End reconnecting with built in error
-      return undefined;
-    }
-    // reconnect after
-    return Math.min(options.attempt * 100, 3000);
+  retry_strategy(retries) {
+    return Math.min(retries * 50, 500);
   }
 
   displayTable(client, severity = "DEBUG") {
