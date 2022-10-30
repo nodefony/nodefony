90e11d1935f98b6a17224e75c9a7953f4388da1a
Christophe CAMENSULI <ccamensuli@gmail.com>
2022-10-30 11:28:17 +0100

    fix(monitoring): clean comments

--- src/nodefony/bundles/monitoring-bundle/src/App.vue
+++ src/nodefony/bundles/monitoring-bundle/src/App.vue
@@ -6,30 +6,6 @@
 	         @error="onError" />
 
 	<v-main ref="main">
-		<!--v-container v-if="$apollo.loading"
-		             style="height: 400px;">
-			<v-row class="fill-height"
-			       align-content="center"
-			       justify="center">
-				<v-row class="text-subtitle-1 text-center">
-					<v-img class=""
-					       width="100"
-					       height="91"
-					       src="/framework-bundle/images/nodefony-logo.png" />
-				</v-row>
-				<v-col class="text-subtitle-1 text-center"
-				       cols="12">
-					Getting Nodefony Monitoring
-				</v-col>
-				<v-col cols="6">
-					<v-progress-linear color="deep-purple accent-4"
-					                   indeterminate
-					                   rounded
-					                   height="6">
-					</v-progress-linear>
-				</v-col>
-			</v-row>
-		</v-container-->
 		<router-view v-if="profile"
 		             :key="$route.fullPath">
 		</router-view>
