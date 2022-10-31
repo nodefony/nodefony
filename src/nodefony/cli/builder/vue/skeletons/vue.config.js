ff70718a462139163ba2b3c66c163eaa35f0c331
Christophe CAMENSULI <ccamensuli@gmail.com>
2022-10-31 20:12:02 +0100

    build: upgrade deps

--- src/nodefony/cli/builder/vue/skeletons/vue.config.js
+++ src/nodefony/cli/builder/vue/skeletons/vue.config.js
@@ -1,18 +1,39 @@
 // vue.config.js
 const path = require('path');
-const package = require(path.resolve("package.json"));
+const Package = require(path.resolve("package.json"));
 const packageVue = require(path.resolve("node_modules","vue","package.json"));
 const outputDir = path.resolve("Resources", "public");
 const indexPath = path.resolve("Resources", "views", 'index.html.twig');
 const publicPath = "/{{bundleName}}";
 const template = path.resolve('public', 'index.html');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const title = package.name;
+const title = Package.name;
+const nodeModule = path.resolve(process.cwd(), "node_modules");
 
-process.env.VUE_APP_VERSION = package.version;
+process.env.VUE_APP_VERSION = Package.version;
 process.env.VUE_APP_VUE_VERSION = packageVue.version;
 process.env.VUE_APP_DEBUG = process.env.DEBUG_MODE;
 process.env.VUE_APP_NODE_ENV = process.env.NODE_ENV;
+try {
+  process.env.VUE_APP_DOMAIN = kernel.domain;
+  process.env.VUE_APP_HTTP_PORT = kernel.httpPort;
+  process.env.VUE_APP_HTTPS_PORT = kernel.httpsPort;
+} catch (e) {}
+
+
+let dev = true;
+let debug = false
+let watch = false
+
+try {
+  if (kernel.environment !== "dev") {
+    dev = false;
+  } else {
+    watch = true;
+  }
+  debug = kernel.debug ? "*" : false;
+} catch (e) {}
+
 
 module.exports = {
   lintOnSave: false,
@@ -26,14 +47,45 @@ module.exports = {
       .tap(args => {
         args[0].title = title;
         args[0].template = template;
+        args[0].chunks = ['app'];
         return args;
       });
   },
   configureWebpack: {
     devtool: process.env.NODE_ENV === "development" ? "source-map" : false,
+    watchOptions: {
+      aggregateTimeout: 2000,
+      ignored: /node_modules|assets|Resources|dist|tmp|public/,
+      followSymlinks: false
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 11000000,
+      maxAssetSize: 11000000
+    },
+    optimization: {
+      runtimeChunk: true,
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false,
+    },
     output:{
-      hotUpdateChunkFilename:'hot/[id].[hash].hot-update.js',
-      hotUpdateMainFilename: 'hot/[hash].hot-update.json'
+      hotUpdateChunkFilename: 'hot/[id].[fullhash].hot-update.js',
+      hotUpdateMainFilename: 'hot/[runtime].[fullhash].hot-update.json'
+    },
+    resolve: {
+      alias: {
+        "@bundles": path.join(__dirname, ".."),
+        "@app": path.join(__dirname, "..", "..", "..", "app"),
+        "vue": path.resolve(`./node_modules/vue`),
+        "vue-router": path.resolve(`./node_modules/vue-router`)
+      },
+      extensions: ['.js', '.json', '.css', '.mjs'],
+      fallback: {
+        "path": false,
+        "assert": false
+      },
+      modules: [nodeModule]
     },
     plugins: [
       new CleanWebpackPlugin({
@@ -45,7 +97,11 @@ module.exports = {
         protectWebpackAssets:true,
         cleanAfterEveryBuildPatterns:[],
         dangerouslyAllowCleanPatternsOutsideProject: true
-      })
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.NODE_DEBUG': JSON.stringify(debug)
+      }),
     ]
   }{% if addons.vuetify %},
   transpileDependencies: [
