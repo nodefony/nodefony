5a97cc7f600b2e4cb496a56735b930548728336c
Christophe CAMENSULI <ccamensuli@gmail.com>
2022-12-29 17:02:09 +0100

    fix(cli): add test with fetch service

--- src/nodefony/cli/builder/skeletons/unittest/testFile.js
+++ src/nodefony/cli/builder/skeletons/unittest/testFile.js
@@ -1,47 +1,46 @@
-/*
- *   MODEFONY FRAMEWORK UNIT TEST
- *
- *   MOCHA STYLE
- *
- *   In the global context you can find :
- *
- *	nodefony : namespace to get library
- *	kernel :   instance of kernel who launch the test
- *
- *
- *
- */
-const assert = require('assert');
+ /*
+  *   MODEFONY FRAMEWORK UNIT TEST
+  *
+  *   MOCHA STYLE
+  *
+  *   In the global context you can find :
+  *
+  *	nodefony : namespace to get library
+  *	kernel :   instance of kernel who launch the test
+  *
+  *
+  *
+  */
+ const assert = require('assert');
+ const https = require('node:https');
 
-describe("BUNDLE {{testName}}", () => {
-  beforeEach(() => {
-    const requestClient = kernel.get("requestClient");
-    const httpsServer = kernel.get("httpsServer");
-    const certificats = httpsServer.getCertificats();
-    const defaultOptions = {
-      method: 'GET',
-      timeout: 5000,
-      agentOptions: {
-        cert: certificats.cert,
-        key: certificats.key,
-        ca: certificats.ca
-      }
-    };
-    const myurl = `https://${kernel.settings.system.domain}:${kernel.settings.system.httpsPort}`;
-    global.request = requestClient.create(myurl, defaultOptions);
-  });
+ describe("BUNDLE {{testName}}", () => {
+   beforeEach(() => {
+     const fetchService = kernel.get("fetch");
+     const httpsServer = kernel.get("httpsServer");
+     const certificats = httpsServer.getCertificats();
+     const httpsAgent = new https.Agent({
+ 	      keepAlive: true,
+         cert: certificats.cert,
+         key: certificats.key,
+         ca: certificats.ca
+       });
+     const defaultOptions = {
+       method: 'GET',
+       agent: httpsAgent
+     };
+     global.url = `https://${kernel.settings.system.domain}:${kernel.settings.system.httpsPort}`;
+     global.fetch = fetchService.fetch
+     global.options = defaultOptions
+   });
 
-  describe('ROUTE', () => {
-
-    it("ROUTE {{routeName}} ", async () => {
-      let options = {
-        timeout: 1500
-      };
-      let result = await global.request.http("{{routeName}}", options);
-      assert.equal(result.json.statusCode, 200);
-      assert.equal(result.json.headers.server, "nodefony");
-    });
-
-  });
-
-});
+   describe('REQUEST ', () => {
+     it("FETCH REQUEST {{routeName}}", async () => {
+       let response = await global.fetch(`${global.url}/{{routeName}}`, global.options)
+       assert.equal(response.status, 200);
+       assert.equal(response.headers.get("server"), "nodefony");
+       let html = await response.text()
+       assert(html);
+     });
+   });
+ });
