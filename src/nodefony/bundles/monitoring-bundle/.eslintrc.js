93c8e6d97bf01ed2ccef39b411be98c90cde5466
Christophe CAMENSULI <ccamensuli@gmail.com>
2023-01-16 13:03:33 +0100

    build: upgrade deps

--- src/nodefony/bundles/monitoring-bundle/.eslintrc.js
+++ src/nodefony/bundles/monitoring-bundle/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
+  plugins: ["prettier"],
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
